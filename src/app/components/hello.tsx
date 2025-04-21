0d0645c14c9fde223c4998994e072afe63203d55
kdragon0211 <johnwilliam05193@gmail.com>
2025-04-22 01:09:44 +0900

    change

---
+++ src/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+function Hello(){
+    console.log("I am a client Component");
+
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default Hello;
